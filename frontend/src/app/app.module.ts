c10b48effa9dc313641deac5d8cb04fd0e63b301
abrar zahin <abrarzahin@yahoo.com>
2019-06-05 20:13:26 -0400

    3 angular post form

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,13 +3,18 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule , MatButtonModule 
+    BrowserModule, BrowserAnimationsModule , 
+    MatButtonModule ,MatInputModule,MatCardModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
