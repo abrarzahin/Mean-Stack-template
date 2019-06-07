2e0a8d072d99814ef17116424e8c5864633123b1
abrar zahin <abrarzahin@yahoo.com>
2019-06-07 19:42:53 -0400

    6 connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
 
 
 const app = express();
 const port = 3000;
+const url = 'mongodb://localhost:27017';
+const dbName = 'messageBoard';
+let db;
 
 app.use(bodyParser.text());
 app.use(cors());
 
 app.post('/api/message',(req,res)=>{
     console.log(req.body);
+    db.collection('messages').insertOne({'msg': req.body});
     res.status(200).send();
 })
+
+MongoClient.connect(url, function (err, client) {
+
+  if(err) return console.log('mongodb error', err);
+
+  console.log("Connected successfully to server");
+
+   db = client.db(dbName);
+  
+});
 
 app.listen(port,()=>console.log('App running on port',port));
